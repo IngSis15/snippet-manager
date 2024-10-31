1b2078273c4b0d6473c821ad84e5bfdc3b459e80
LautiGR_116 <lreinoso@mail.austral.edu.ar>
2024-10-31 17:16:08 -0300

    ktlint

--- src/main/kotlin/edu/ingsis/snippetmanager/test/Test.kt
+++ src/main/kotlin/edu/ingsis/snippetmanager/test/Test.kt
@@ -1,7 +1,15 @@
 package edu.ingsis.snippetmanager.test
 
 import edu.ingsis.snippetmanager.snippet.Snippet
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
 
 @Entity
 data class Test(
