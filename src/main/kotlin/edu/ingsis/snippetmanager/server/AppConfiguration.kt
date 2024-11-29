799c41c20cfb9d6a9474c67361b2429be35025d3
alexk <akottner@mail.austral.edu.ar>
2024-11-29 11:26:36 -0300

    new relic and interceptor

---
+++ src/main/kotlin/edu/ingsis/snippetmanager/server/AppConfiguration.kt
@@ -0,0 +1,15 @@
+package edu.ingsis.snippetmanager.server
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class AppConfiguration(private val correlationIdInterceptor: CorrelationIdInterceptor) {
+    @Bean
+    fun restTemplate(): RestTemplate {
+        val restTemplate = RestTemplate()
+        restTemplate.interceptors.add(correlationIdInterceptor)
+        return restTemplate
+    }
+}
